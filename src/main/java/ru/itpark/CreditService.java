c428c3f65636cffb8b4e8147d40debfc7af82d39
pentogono <ilg3109@gmail.com>
2018-12-03 22:31:53 +0300

    Исправление ошибок

--- src/main/java/ru/itpark/CreditService.java
+++ src/main/java/ru/itpark/CreditService.java
@@ -3,15 +3,12 @@ package ru.itpark;
 public class CreditService {
     public int calculatePayment(int creditAmount, int creditTerm, int loanRate) {
 
-        int constantsOne = 1;
-        int monthYear = 12;
-        int hundredPercent = 100;
-        double percentageRate = (double) loanRate / (double) hundredPercent;
-        double monthRate = percentageRate / (double) monthYear;
-        double baseDegree = (double) constantsOne + monthRate;
+        double percentageRate = (double) loanRate / 100;
+        double monthRate = percentageRate / (double) 12;
+        double baseDegree = (double) 1 + monthRate;
         double exponentiation = Math.pow(baseDegree, creditTerm);
 
-        int monthlyPayment = (int) (((monthRate / (exponentiation - constantsOne)) + monthRate) * creditAmount);
+        int monthlyPayment = (int) (((monthRate / (exponentiation - 1)) + monthRate) * creditAmount);
 
         return monthlyPayment;
     }
